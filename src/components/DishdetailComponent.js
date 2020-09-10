ce7f80b1e264b886b0bb32f747d7e1e2151c4e60
Kausthub Thekke Madathil <kausthubtm6671@gmail.com>
2020-09-10 20:46:57 +0530

    uploading files

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { CardBody, CardImg, CardText, CardTitle, Card, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom'
+
+
+
+    function RenderDish({dish}) {
+        if (dish!=null) {
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle><b>{dish.name}</b></CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    function RenderComments({comments}) {
+        if (comments != null)
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>
+                                    -- {comment.author},{' '}
+                                    {new Intl.DateTimeFormat('en-US', {
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: '2-digit'
+                                    }).format(new Date(comment.date))}
+                                </p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            );
+        else return(
+            <div></div>
+        );
+    }
+       
+
+    const DishDetails = (props) => {
+
+        if (props.dish != null) 
+            return (
+                <div className='container'>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+            ); 
+        else return (
+            <div></div>
+        );
+    }
+
+
+export default DishDetails;
